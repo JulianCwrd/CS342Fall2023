618ee7978a37ea81a1330b718cc041f747ebcc3a
Julian Coward <juliancoward54@gmail.com>
2023-09-27 21:10:21 -0500

    Added GenericList

---
+++ Projects/GLMaven_Project1_Fall2023/src/main/java/GenericList.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Collections;
+abstract class GenericList<T> implements Iterabler<T>{
+    //Interator<T> iterator();
+    //head (this is the head of the list and should be private).
+    //private  Node<T>;
+   // private int length; // (the length of the list and should be private)
+}
