37945c70c3c1e829e6f77c33a8eec690599883cd
Julian Coward <juliancoward54@gmail.com>
2023-10-07 21:42:27 -0500

    Finishing homework 2, main functions implemented now doing aesthetic changes

--- Homeworks/HomeworkThree/Fall23_Homework3_mvn/src/main/java/JavaFXTemplate.java
+++ Homeworks/HomeworkThree/Fall23_Homework3_mvn/src/main/java/JavaFXTemplate.java
@@ -1,11 +1,12 @@
 import javafx.application.Application;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
@@ -22,21 +23,42 @@ public class JavaFXTemplate extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
 		primaryStage.setTitle("Julian Coward Homework 3");
+
 		Button btn1 = new Button("button 1");
 		Button btn2 = new Button("button 2");
+
 		BorderPane borderPane = new BorderPane();
 		VBox buttons = new VBox(50,btn1, btn2);
+
 		TextField txt1 = new TextField("enter text here then press button 1");
 		TextField txt2 = new TextField("final string goes here");
+		txt2.setEditable(false);
+
+		btn1.setOnAction(new EventHandler<ActionEvent>(){
+			public void handle(ActionEvent action){
+				txt2.setText(txt1.getText() + " : from the center text field!");
+				btn1.setDisable(true);
+				btn1.setText("pressed");
+			}
+		});
+
+		btn2.setOnAction(e -> {
+			txt1.clear();
+			txt2.clear();
+			txt2.setText("final string goes here");
+			btn1.setDisable(false);
+			btn1.setText("button 1");
+		});
+
 		borderPane.setCenter(txt1);
-		txt1.setAlignment(Pos.CENTER);
 		borderPane.setRight(txt2);
 		borderPane.setLeft(buttons);
-		txt1.setMaxWidth(250);
-		txt2.setEditable(false);
+		txt2.setAlignment(Pos.CENTER);
+
 		Scene scene = new Scene(borderPane, 700,700);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
+
 
 }
